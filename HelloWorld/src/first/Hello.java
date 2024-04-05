556c2edec616916a10ca83f93f3debfafeff16da
tokyogiants <yuki.takahashi.b138@dirbato.co.jp>
2024-04-05 17:16:54 +0900

    Change-Id: I0000000000000000000000000000000000000000
    Change-Id: I2e6b8943edd437bd869c72f4363ed43dc19cfae0

--- HelloWorld/src/first/Hello.java
+++ HelloWorld/src/first/Hello.java
@@ -3,10 +3,8 @@ package first;
 public class Hello {
 
 	public static void main(String[] args) {
-System.out.println("Hello,world!");
-System.out.println("こんにちは、世界！")
-	System.out.println("こんにちは、世界！"
-			);}
+		System.out.println("Hello,world!");
+		System.out.println("こんにちは、世界！");
+	}
 	
-
 }
