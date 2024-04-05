17152591ef12c54fce688ec6deab84be3fdba515
tokyogiants <yuki.takahashi.b138@dirbato.co.jp>
2024-04-05 16:33:28 +0900

    練習してます

--- HelloWorld/src/first/Hello.java
+++ HelloWorld/src/first/Hello.java
@@ -5,7 +5,8 @@ public class Hello {
 	public static void main(String[] args) {
 System.out.println("Hello,world!");
 System.out.println("こんにちは、世界！")
-	}
+	System.out.println("こんにちは、世界！"
+			);}
 	
 
 }
